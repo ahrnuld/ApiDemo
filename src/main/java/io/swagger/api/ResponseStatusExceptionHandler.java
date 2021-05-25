a69bbdc2819af2adc30e236cee6f3626d480fd08
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-05-25 23:05:32 +0200

    Added some exception handling

--- src/main/java/io/swagger/api/ResponseStatusExceptionHandler.java
+++ src/main/java/io/swagger/api/ResponseStatusExceptionHandler.java
@@ -1,6 +1,7 @@
 package io.swagger.api;
 
 import io.swagger.model.DTO.ExceptionDTO;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +11,34 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.naming.AuthenticationException;
+import javax.security.auth.message.AuthException;
+import java.util.logging.Logger;
+
 @ControllerAdvice
 public class ResponseStatusExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = { ResponseStatusException.class })
+    private final Logger log = Logger.getLogger(this.getClass().getName());
+
+    @Order(1)
+    @ExceptionHandler(value = {ResponseStatusException.class})
     protected ResponseEntity<Object> handleResponseStatusException(ResponseStatusException ex, WebRequest request) {
         ExceptionDTO dto = new ExceptionDTO(ex.getReason());
         return handleExceptionInternal(ex, dto, new HttpHeaders(), HttpStatus.valueOf(ex.getStatus().value()), request);
+    }
+
+    @Order(2)
+    @ExceptionHandler(value = {AuthException.class})
+    protected ResponseEntity<Object> handleAuthenticationException(AuthenticationException ex, WebRequest request) {
+        ExceptionDTO dto = new ExceptionDTO(ex.getMessage());
+        return handleExceptionInternal(ex, dto, new HttpHeaders(), HttpStatus.FORBIDDEN, request);
+    }
+
+    @Order(3)
+    @ExceptionHandler(value = {Exception.class})
+    protected ResponseEntity<Object> handleTheRest(Exception ex, WebRequest request) {
+        log.info(ex.getClass().getName());
+        ExceptionDTO dto = new ExceptionDTO(ex.getMessage());
+        return handleExceptionInternal(ex, dto, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 }
--- src/main/java/io/swagger/service/UserService.java
+++ src/main/java/io/swagger/service/UserService.java
@@ -7,14 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class UserService {
@@ -33,13 +28,9 @@ public class UserService {
 
     public String login(String username, String password)
     {
-        try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
             User user = userRepository.findByUsername(username);
             return jwtTokenProvider.createToken(username, user.getRoles());
-        } catch(AuthenticationException ex) {
-            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Username/password invalid");
-        }
     }
 
     public User add(User user){
